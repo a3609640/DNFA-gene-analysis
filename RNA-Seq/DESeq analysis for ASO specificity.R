9b0207b2ea36543d69fa0def06720b236230ab3f
a3609640 <wusu2014@gmail.com>
2018-10-27 19:05:37 -0400

    Create LICENSE and NOTICE files.

--- RNA-Seq/DESeq analysis for ASO specificity.R
+++ RNA-Seq/DESeq analysis for ASO specificity.R
@@ -64,7 +64,7 @@ library(plotly)
 # obtain the count table of the experiment directly from a pre-saved file: testseq.csv. 
 # The RNA-seq was aligned to human reference genome Hg38 by STAR aligner
 # read processed RNA-seq read data from file testseq.csv.
-testseqCSV <- "./Test run/Analysis/STAR/results/testseq.csv"
+testseqCSV <- "./Analysis/STAR/results/testseq.csv"
 testseq <- read.csv(testseqCSV)
 # Use the column one (Ensemble names) as columnn names. 
 testseq <- data.frame(testseq[,-1], row.names=testseq[,1])
