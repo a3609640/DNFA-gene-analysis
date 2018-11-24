3e23a761e2ff01f62d5bf2960eab6b407c6dc77b
Su Wu <wusu2014@gmail.com>
2018-11-24 17:48:07 -0500

    update

--- R/Xena.R
+++ R/Xena.R
@@ -1,4 +1,4 @@
-## the following script perform PCA on RNA-Seq data of seven DNFA genes from SKCM amd GTEX dataset with R package "ggfortify".  
+## the following script perform PCA on RNA-Seq data of seven DNFA genes from SKCM amd GTEX dataset with R package "ggfortify".
 library(cluster)
 library(ggfortify)
 library(lfda)
@@ -16,7 +16,7 @@ str(DNFASKCMandGTEX)
 DNFASKCMandGTEX<-as.data.frame(DNFASKCMandGTEX)
 DNFASKCMandGTEX$sample_type<-as.factor(DNFASKCMandGTEX$sample_type)
 
-DNFASKCMandGTEX$sample_type <- factor(DNFASKCMandGTEX$sample_type, 
+DNFASKCMandGTEX$sample_type <- factor(DNFASKCMandGTEX$sample_type,
                                       levels = c("Normal Tissue",
                                                  "Primary Tumor",
                                                  "Metastatic"))
@@ -31,14 +31,8 @@ list(DNFASKCMandGTEX)
 # we remove the 8th column from DNFASKCMandGTEX data
 df <- DNFASKCMandGTEX[-8]
 autoplot(prcomp(df), data = DNFASKCMandGTEX, colour = 'sample_type')
-<<<<<<< HEAD
 # we can color each data point in the plot according to their cancer category: colour = 'sample_type'
 # Use loadings = TRUE, we draw eigenvector for each DNFA gene on the plot.
-=======
-# we can color each data point in the plot according to their cancer category:
-# colour = 'sample_type'
-# Using loadings = TRUE, we draw eigenvector for each DNFA gene on the plot. 
->>>>>>> d4213730f6fc2bca69f909b3b57c224551df321c
 bp = autoplot(prcomp(df), data = DNFASKCMandGTEX, colour = 'sample_type',
          loadings = TRUE, loadings.colour = 'black',
          loadings.label.vjust = -1,
@@ -61,11 +55,11 @@ bp
 PC<-prcomp(df)
 PCi<-data.frame(PC$x,Species=DNFASKCMandGTEX$sample_type)
 ggplot(PCi,aes(x=PC1,y=PC2,col=Species))+
-  geom_point(size=3,alpha=0.5)+ 
+  geom_point(size=3,alpha=0.5)+
   scale_color_manual(values = c("#FF1BB3","#A7FF5B","#99554D","#CC0000"))+ #choose colors here
   theme_classic()
 
-# clara: Clustering Large Applications, we chose the number of clusters as "3". 
+# clara: Clustering Large Applications, we chose the number of clusters as "3".
 # tumor tissues and healthy tissus samples are clearly seperated,
 # However, metastatic and primary tumor samples can not be seperated by clustering method
 # It is required that 0 < k < n where n is the number of factors. Here 4 categories of tissues
