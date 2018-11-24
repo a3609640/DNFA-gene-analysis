c51aa8fcada4c6ccd2b818a6ecf786e24cf88513
David L. Roxe <dlroxe@google.com>
2018-11-24 01:11:36 -0600

    several whitespace adjustments to appease RStudio lint warnings

--- R/scRNA-Seq.R
+++ R/scRNA-Seq.R
@@ -18,22 +18,22 @@ library(plyr)
 get_lipogenesis_data <- function(data) {
   # -----------------------------------------------------------------------------------------------
   #select the genes of interest
-  SREBF1 <- data[data$Cell=="SREBF1",]
-  SREBF2 <- data[data$Cell=="SREBF2",]
-  FASN <- data[data$Cell=="FASN",]
-  SCD <- data[data$Cell=="SCD",]
-  ACACA <- data[data$Cell=="ACACA",]
-  ACSS2 <- data[data$Cell=="ACSS2",]
-  ACSL1 <- data[data$Cell=="ACSL1",]
-  ACLY <- data[data$Cell=="ACLY",]
-  HMGCR <- data[data$Cell=="HMGCR",]
-  HMGCS1 <- data[data$Cell=="HMGCS1",]
-  PPARGC1A <- data[data$Cell=="PPARGC1A",]
-  PPARGC1B <- data[data$Cell=="PPARGC1B",]
-  MITF <- data[data$Cell=="MITF",]
-  AXL <- data[data$Cell=="AXL",]
-  tumor <- data[data$Cell=="tumor",]
-  malignant <- data[data$Cell=="malignant(1=no,2=yes,0=unresolved)",]
+  SREBF1 <- data[data$Cell == "SREBF1",]
+  SREBF2 <- data[data$Cell == "SREBF2",]
+  FASN <- data[data$Cell == "FASN",]
+  SCD <- data[data$Cell == "SCD",]
+  ACACA <- data[data$Cell == "ACACA",]
+  ACSS2 <- data[data$Cell == "ACSS2",]
+  ACSL1 <- data[data$Cell == "ACSL1",]
+  ACLY <- data[data$Cell == "ACLY",]
+  HMGCR <- data[data$Cell == "HMGCR",]
+  HMGCS1 <- data[data$Cell == "HMGCS1",]
+  PPARGC1A <- data[data$Cell == "PPARGC1A",]
+  PPARGC1B <- data[data$Cell == "PPARGC1B",]
+  MITF <- data[data$Cell == "MITF",]
+  AXL <- data[data$Cell == "AXL",]
+  tumor <- data[data$Cell == "tumor",]
+  malignant <- data[data$Cell == "malignant(1=no,2=yes,0=unresolved)",]
 
   # -----------------------------------------------------------------------------------------------
   #combine them into a new table "geneset"
@@ -47,7 +47,7 @@ get_lipogenesis_data <- function(data) {
   colnames(transposed_geneset) <- n
   renamed_geneset <- rename(
     transposed_geneset,
-    c('malignant(1=no,2=yes,0=unresolved)'='malignancy'))
+    c('malignant(1=no,2=yes,0=unresolved)' = 'malignancy'))
   renamed_geneset$malignancy <- factor(renamed_geneset$malignancy)
   renamed_geneset$malignancy <- revalue(renamed_geneset$malignancy,
                                         c("0" = "Unresolved",
@@ -80,41 +80,41 @@ nonmalignantgeneset <- subset(lipogenesis_data, malignancy == "Non-malignant")
 ###############################################################################
 ggplot(totalgeneset, aes(x=factor(tumor), y=SREBF1)) +
   geom_boxplot(size = 1,
-               outlier.colour=NA,
-               color="black",  # the line for boxplot is in black color. 
+               outlier.colour = NA,
+               color = "black",  # the line for boxplot is in black color. 
                aes(fill = malignancy)) +
   labs(x = "tumor samples", y = "SREBF1 mRNA counts") +
-  theme_bw()+
-  theme(axis.title=element_text(face="bold",size=18,color="black"),
-        axis.text=element_text(size=18,face="bold",color="black"),
+  theme_bw() +
+  theme(axis.title = element_text(face = "bold", size = 18, color = "black"),
+        axis.text = element_text(size = 18,face = "bold", color = "black"),
         axis.title.x = element_text(margin = unit(c(5, 0, 0, 0), "mm")),
         axis.title.y = element_text(margin = unit(c(0, 5, 0, 0), "mm")),
-        axis.line.x = element_line(color="black", size=1),
-        axis.line.y = element_line(color="black", size=1),
+        axis.line.x = element_line(color = "black", size = 1),
+        axis.line.y = element_line(color = "black", size = 1),
         axis.ticks = element_line(size = 1),
         axis.ticks.length = unit(.25, "cm"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank(),
-        legend.text = element_text(size=18,face="bold",colour = 'black'),
-        legend.position=c(0,1),
-        legend.justification=c(-0.1,1.1),
+        legend.text = element_text(size = 18, face = "bold", colour = 'black'),
+        legend.position = c(0,1),
+        legend.justification = c(-0.1,1.1),
         legend.key.height = unit(2.2, 'lines'),
         legend.background = element_rect(fill = "white")) +
-  guides(fill=guide_legend(title=NULL)) +
-  scale_y_continuous(breaks=seq(0, 10, 2))+
-  scale_fill_discrete(labels=c("Nonmalignant cells", "Malignant cells"))
+  guides(fill=guide_legend(title = NULL)) +
+  scale_y_continuous(breaks = seq(0, 10, 2)) +
+  scale_fill_discrete(labels = c("Nonmalignant cells", "Malignant cells"))
 
 # -----------------------------------------------------------------------------------------------
-ggplot(totalgeneset, aes(x=factor(tumor), y=FASN)) +
+ggplot(totalgeneset, aes(x=factor(tumor), y = FASN)) +
   geom_boxplot(size = 1,
-               outlier.colour=NA,
-               color="black",
+               outlier.colour = NA,
+               color = "black",
                aes(fill = malignancy)) +
   labs(x = "tumor samples", y = "FASN mRNA counts") +
-  theme_bw()+
-  theme(axis.title=element_text(face="bold",size=18,color="black"),
+  theme_bw() +
+  theme(axis.title=element_text(face = "bold", size = 18, color = "black"),
         axis.text=element_text(size=18,face="bold",color="black"),
         axis.title.x = element_text(margin = unit(c(5, 0, 0, 0), "mm")),
         axis.title.y = element_text(margin = unit(c(0, 5, 0, 0), "mm")),
