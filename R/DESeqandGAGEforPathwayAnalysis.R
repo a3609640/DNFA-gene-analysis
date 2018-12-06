c47acdff92a84471683195922669d74ac4c98839
Su Wu <wusu2014@gmail.com>
2018-12-05 23:30:25 -0500

    more style changes. still need to introduce path to the input xls file.

--- R/DESeqandGAGEforPathwayAnalysis.R
+++ R/DESeqandGAGEforPathwayAnalysis.R
@@ -49,13 +49,13 @@ data(kegg.gs)
 
 # TODO(dlroxe): This function is copied from the FactoMineR file.
 # Find a single place to define it in common for all callers.
-.readGeneCounts <- function () {
+.readGeneCounts <- function() {
   # obtain the count table of the experiment directly from a pre-saved file: gene-counts.csv.
   # The RNA-seq was aligned to human reference genome Hg38 by STAR aligner
   # read processed RNA-seq read data from file testseq.csv.
   testseq <- read.csv(file.path("project-data", "gene-counts-from-Makefile.csv"))
   # Use the column one (Ensemble names) as columnn names.
-  testseq <- data.frame(testseq[,-1], row.names=testseq[,1])
+  testseq <- data.frame(testseq[,-1], row.names = testseq[,1])
   # Remove the first four rows (N_unmapped,N_multimapping,N_noFeature and N_ambiguous)
   testseq <- data.frame(testseq[c(-1,-2,-3,-4),])
 
@@ -74,24 +74,24 @@ data(kegg.gs)
 # found in FactoMineR
 .getGuideData <- function(testseq) {
   ## check the distribution of RNA-Seq reads
-  par(mar=c(3,12,2,1))
-  boxplot(testseq, outline=FALSE, horizontal=TRUE, las=1)
+  par(mar = c(3,12,2,1))
+  boxplot(testseq, outline = FALSE, horizontal = TRUE, las = 1)
   ## Remove rows in which there are no reads or nearly no reads
-  guideData <- testseq[rowSums(testseq)>1,]
+  guideData <- testseq[rowSums(testseq) > 1,]
   head(guideData)
   dim(guideData)
   ## check how the data distribution with boxplot after removing rows with no read
-  par(mar=c(3,12,2,1))
-  boxplot(guideData, outline=FALSE, horizontal=TRUE, las=1)
+  par(mar = c(3,12,2,1))
+  boxplot(guideData, outline = FALSE, horizontal = TRUE, las = 1)
   return(guideData)
 }
 
 # TODO(dlroxe): again, same function as other file
-.getDDS <- function (guideData, guideDesign, condition) {
+.getDDS <- function(guideData, guideDesign, condition) {
   ## Construct DESeqDataSet with the count matrix, countData, and the sample information, colData
   dds <- DESeqDataSetFromMatrix(countData = guideData,
-                                colData = guideDesign,
-                                design = ~ condition)
+                                colData   = guideDesign,
+                                design    = ~ condition)
   dds
   head(assay(dds))
   return(dds)
@@ -105,7 +105,7 @@ data(kegg.gs)
 ## 4.1 standard analysis to make MA-plot from base means and log fold changes##
 ###############################################################################
 .getDDSRes <- function(ddsDEsiRNA) {
-  ressiRNA<-results(ddsDEsiRNA) # default alpha = 0.1
+  ressiRNA <- results(ddsDEsiRNA) # default alpha = 0.1
   ## p-values and adjusted p-values
   ## We can order our results table by the smallest p value:
   # ressiRNA<-ressiRNA[order(ressiRNA$log2FoldChange),]
@@ -115,26 +115,29 @@ data(kegg.gs)
   ## We can summarize some basic tallies using the summary function
   summary(ressiRNA)
   ## How many adjusted p-values were less than 0.1?
-  sum(ressiRNA$padj < 0.1, na.rm=TRUE)
+  sum(ressiRNA$padj < 0.1, na.rm = TRUE)
 
   ## Note that the results function automatically performs independent filtering based on the mean of normalized counts for each gene,
   ## optimizing the number of genes which will have an adjusted p value below a given FDR cutoff, alpha = 0.05 here
-  res05 <- results(ddsDEsiRNA, alpha=0.05)
+  res05 <- results(ddsDEsiRNA, alpha = 0.05)
   summary(res05)
-  sum(res05$padj < 0.05, na.rm=TRUE)
-
+  sum(res05$padj < 0.05, na.rm = TRUE)
   ## plotMA shows the log2 fold changes attributable to a given variable over the mean of normalized counts for all the samples in the DESeqDataSet.
   ## Points will be colored red if the adjusted p value is less than 0.1.
   ## Points which fall out of the window are plotted as open triangles pointing either up or down.
-  plotMA(ressiRNA, main="DESeq2 siRNA", ylim=c(-2,2))
+  plotMA(ressiRNA, main = "DESeq2 siRNA", ylim = c(-2,2))
   ## It is more useful visualize the MA-plot for the shrunken log2 fold changes,
   ## which remove the noise associated with log2 fold changes from low count genes without requiring arbitrary filtering thresholds.
   resultsNames(ddsDEsiRNA)
   # normal is the the original DESeq2 shrinkage estimator, an adaptive normal prior
-  resLFC <- lfcShrink(ddsDEsiRNA, coef=2)
+  resLFC <- lfcShrink(ddsDEsiRNA, coef = 2)
   # par(mfrow=c(1,3), mar=c(4,4,2,1))
   xlim <- c(1,1e5); ylim <- c(-2.2,2.2)
-  plotMA(resLFC,xlab = "mean of normalized counts", ylim=ylim, main="normal",cex=.8)
+  plotMA(resLFC,
+         xlab = "mean of normalized counts",
+         ylim = ylim,
+         main = "normal",
+         cex  = .8)
   # plotMA(resLFC, xlim=xlim, xlab = "mean of normalized counts", ylim=ylim, main="normal")
   return(ressiRNA)
 }
@@ -147,20 +150,20 @@ data(kegg.gs)
   ressiRNA <- data.frame(ressiRNA)
   columns(org.Hs.eg.db)
   ressiRNA$symbol = mapIds(org.Hs.eg.db,
-                           keys=row.names(ressiRNA),
-                           column="SYMBOL",
-                           keytype="ENSEMBL",
-                           multiVals="first")
+                           keys      = row.names(ressiRNA),
+                           column    = "SYMBOL",
+                           keytype   = "ENSEMBL",
+                           multiVals = "first")
   ressiRNA$entrez = mapIds(org.Hs.eg.db,
-                           keys=row.names(ressiRNA),
-                           column="ENTREZID",
-                           keytype="ENSEMBL",
-                           multiVals="first")
+                           keys      = row.names(ressiRNA),
+                           column    = "ENTREZID",
+                           keytype   = "ENSEMBL",
+                           multiVals = "first")
   ressiRNA$name =   mapIds(org.Hs.eg.db,
-                           keys=row.names(ressiRNA),
-                           column="GENENAME",
-                           keytype="ENSEMBL",
-                           multiVals="first")
+                           keys      = row.names(ressiRNA),
+                           column    = "GENENAME",
+                           keytype   = "ENSEMBL",
+                           multiVals = "first")
   summary(ressiRNA)
   head(ressiRNA, 10)
   return(ressiRNA)
@@ -175,9 +178,9 @@ data(kegg.gs)
   resSig <- subset(ressiRNA, pvalue < 0.05)
   resSig
   write.csv(as.data.frame(resSig),
-            file="siBF1_siNeg_sigresults.csv")
+            file = "siBF1_siNeg_sigresults.csv")
   write.csv(as.data.frame(ressiRNA),
-            file="siBF1_siNeg_results.csv")
+            file = "siBF1_siNeg_results.csv")
 }
 
 ##############################################################
@@ -190,15 +193,23 @@ data(kegg.gs)
 # So these are different ways of ranking genes.
 # calculate the variance for each gene, # select the top 50 genes by variance
 .makeHeatMap2 <- function(ddsDEsiRNA) {
-  rldsiRNA <- rlog(ddsDEsiRNA,blind=TRUE)
-  vdsiRNA <- varianceStabilizingTransformation(ddsDEsiRNA, blind=TRUE)
-  topVarGenes <- head(order(rowVars(assay(vdsiRNA)), decreasing=TRUE), 500)
+  rldsiRNA <- rlog(ddsDEsiRNA,blind = TRUE)
+  vdsiRNA <- varianceStabilizingTransformation(ddsDEsiRNA, blind = TRUE)
+  topVarGenes <- head(order(rowVars(assay(vdsiRNA)), decreasing = TRUE), 500)
   mat <- assay(rldsiRNA)[topVarGenes, ]
   mat <- mat - rowMeans(mat)
-  heatmap.2(mat,labRow = NA,labCol=NA, scale="row",lhei = c(2, 8),
-            trace="none", dendrogram="none", margins=c(2,10),
-            col = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
-            ColSideColors = c(Control="gray", DPN="darkgreen", OHT="orange")[colData(rldsiRNA)$condition ])
+  heatmap.2(mat,
+            labRow        = NA,
+            labCol        = NA,
+            scale         = "row",
+            lhei          = c(2, 8),
+            trace         = "none",
+            dendrogram    = "none",
+            margins       = c(2,10),
+            col           = colorRampPalette( rev(brewer.pal(9, "RdBu")) )(255),
+            ColSideColors = c(Control = "gray",
+                              DPN     = "darkgreen",
+                              OHT     = "orange")[colData(rldsiRNA)$condition ])
 }
 
 ######################################################
@@ -218,26 +229,34 @@ data(kegg.gs)
   head(kegg.sets.hs.sigmet, 3)
   
   ## Generate a named vector of fold changes, where the names of the values are the Entrez gene IDs, for the gage() function
-  foldchanges = ressiRNA$log2FoldChange
-  names(foldchanges) = ressiRNA$entrez
+  foldchanges <- ressiRNA$log2FoldChange
+  names(foldchanges) <- ressiRNA$entrez
   head(foldchanges)
   
   # Look at top upregulated (greater) and downregulated (less) genes with statatistics.
   # Get KEGG pathway with both metabolism and signaling pathways
-  kg.hsa=kegg.gsets("hsa")
-  kegg.sigmet.idx=kg.hsa$kg.sets[kg.hsa$sigmet.idx]
-  keggres.sigmet.idx = gage(foldchanges, gsets=kegg.sigmet.idx, same.dir=TRUE)
+  kg.hsa <- kegg.gsets("hsa")
+  kegg.sigmet.idx <- kg.hsa$kg.sets[kg.hsa$sigmet.idx]
+  keggres.sigmet.idx <- gage(foldchanges,
+                             gsets    = kegg.sigmet.idx,
+                             same.dir = TRUE)
   lapply(keggres.sigmet.idx, head,20)
-  write.table(keggres.sigmet.idx$greater, file = "keggres.sigmet.idx.greater.txt",sep = "\t")
-  write.table(keggres.sigmet.idx$less, file = "keggres.sigmet.idx.less.txt",sep = "\t")
+  write.table(keggres.sigmet.idx$greater,
+              file = "keggres.sigmet.idx.greater.txt",sep = "\t")
+  write.table(keggres.sigmet.idx$less,
+              file = "keggres.sigmet.idx.less.txt",sep = "\t")
 
   # Get KEGG pathway with only metabolism pathways
-  kegg.met.idx=kg.hsa$kg.sets[kg.hsa$met.idx]
-  keggres.met.idx = gage(foldchanges, gsets=kegg.met.idx, same.dir=TRUE)
+  kegg.met.idx <- kg.hsa$kg.sets[kg.hsa$met.idx]
+  keggres.met.idx <- gage(foldchanges,
+                          gsets    = kegg.met.idx,
+                          same.dir = TRUE)
   lapply(keggres.met.idx, head,10)
   # Get KEGG pathway with only signaling pathways
-  kegg.sig.idx=kg.hsa$kg.sets[kg.hsa$sig.idx]
-  keggres.sig.idx = gage(foldchanges, gsets=kegg.sig.idx, same.dir=TRUE)
+  kegg.sig.idx <- kg.hsa$kg.sets[kg.hsa$sig.idx]
+  keggres.sig.idx <- gage(foldchanges,
+                          gsets    = kegg.sig.idx,
+                          same.dir = TRUE)
   lapply(keggres.sig.idx, head,10)
   return(foldchanges)
 }
@@ -249,12 +268,14 @@ data(kegg.gs)
   # set up GO database
   data(go.gs)
   lapply(go.gs[1:3],head)
-  go.hs <- go.gsets(species="human")
+  go.hs <- go.gsets(species = "human")
   go.bp.gs <- go.hs$go.sets[go.hs$go.subs$BP] # “Biological Process”
   go.mf.gs <- go.hs$go.sets[go.hs$go.subs$MF] # “Molecular Function”
   go.cc.gs <- go.hs$go.sets[go.hs$go.subs$CC] # “Cellular Component”
   # GO pathway analysis with Biological Process
-  fc.go.bp.p <- gage(foldchanges, gsets = go.bp.gs,same.dir=TRUE)
+  fc.go.bp.p <- gage(foldchanges,
+                     gsets  = go.bp.gs,
+                     same.dir = TRUE)
   lapply(fc.go.bp.p, head,20)
   write.table(fc.go.bp.p$greater, file = "fc.go.bp.p.greater.txt",sep = "\t")
   write.table(fc.go.bp.p$less, file = "fc.go.bp.p.less.txt",sep = "\t")
@@ -268,32 +289,23 @@ data(kegg.gs)
 }
 
 doAll2 <- function() {
-testseq <- .readGeneCounts()
-guideDatasiRNA <- .getGuideData(
+  testseq <- .readGeneCounts()
+  guideDatasiRNA <- .getGuideData(
   data.frame(testseq[,c(5,6,7,8,9,10,11,12)]))
-
-condition <- c(rep("siNeg",4),rep("siBF1",4))
-guideDesignsiRNA <- data.frame(row.names = colnames(guideDatasiRNA),
-                               condition = condition)
-
+  condition <- c(rep("siNeg",4),rep("siBF1",4))
+  guideDesignsiRNA <- data.frame(row.names = colnames(guideDatasiRNA),
+                                 condition = condition)
 ## specifying the reference level:
-ddssiRNA <- .getDDS(guideDatasiRNA, guideDesignsiRNA, condition)
-ddssiRNA$condition <- relevel(ddssiRNA$condition, ref = "siNeg")
-ddssiRNA
-
-ddsDEsiRNA <- DESeq(ddssiRNA)
-ressiRNA <- .getDDSRes(ddsDEsiRNA)
-
-ressiRNA <- .addGeneIdentifiers(ressiRNA)
-
-.exportCSV(ressiRNA)
-
-.makeHeatMap2(ddsDEsiRNA)
-
-foldchanges <- .keggAnalysis(ressiRNA)
-
-.pathwayAnalysis(foldchanges)
-
+  ddssiRNA <- .getDDS(guideDatasiRNA, guideDesignsiRNA, condition)
+  ddssiRNA$condition <- relevel(ddssiRNA$condition, ref = "siNeg")
+  ddssiRNA
+  ddsDEsiRNA <- DESeq(ddssiRNA)
+  ressiRNA <- .getDDSRes(ddsDEsiRNA)
+  ressiRNA <- .addGeneIdentifiers(ressiRNA)
+  .exportCSV(ressiRNA)
+  .makeHeatMap2(ddsDEsiRNA)
+  foldchanges <- .keggAnalysis(ressiRNA)
+  .pathwayAnalysis(foldchanges)
 }
 
 # TODO(dlroxe): doAll2a() is a temporary wrapper for code that doesn't
@@ -305,19 +317,29 @@ doAll2a <- function() {
 #setwd("~/Documents/Su Wu/Documents/Research/Naar Lab/ChIP-seq")
 # TODO(suwu): check this file into project-data/ ...
 # TODO(dlroxe): ...or generate equivalent data programatically
-ChIP_Seq_and_RNA_Seq_overlap <- read_excel("ChIP-Seq and RNA-Seq overlap.xlsx",
-                                            col_names = FALSE)
-colnames(ChIP_Seq_and_RNA_Seq_overlap) <- "symbol"
-View(ChIP_Seq_and_RNA_Seq_overlap)
-ChIP_Seq_and_RNA_Seq_overlap <- merge(ressiRNA, ChIP_Seq_and_RNA_Seq_overlap, by="symbol")
-CR.foldchanges = -ChIP_Seq_and_RNA_Seq_overlap$log2FoldChange
-names(CR.foldchanges) = ChIP_Seq_and_RNA_Seq_overlap$entrez
-head(CR.foldchanges)
-
-keggres.sigmet.idx = gage(CR.foldchanges, gsets=kegg.sigmet.idx, same.dir=TRUE)
-lapply(keggres.sigmet.idx, head,10)
-fc.go.bp.p <- gage(CR.foldchanges, gsets = go.bp.gs,same.dir=TRUE)
-lapply(fc.go.bp.p, head,20)
-write.table(fc.go.bp.p$greater, file = "ChIP_Seq_and_RNA_Seq_overlap.go.bp.p.greater.txt",sep = "\t")
-write.table(fc.go.bp.p$less, file = "ChIP_Seq_and_RNA_Seq_overlap.go.bp.p.less.txt",sep = "\t")
+  ChIP_Seq_and_RNA_Seq_overlap <- read_excel("ChIP-Seq and RNA-Seq overlap.xlsx",
+                                             col_names = FALSE)
+  colnames(ChIP_Seq_and_RNA_Seq_overlap) <- "symbol"
+  View(ChIP_Seq_and_RNA_Seq_overlap)
+  ChIP_Seq_and_RNA_Seq_overlap <- merge(ressiRNA,
+                                        ChIP_Seq_and_RNA_Seq_overlap,
+                                        by = "symbol")
+  CR.foldchanges <- ChIP_Seq_and_RNA_Seq_overlap$log2FoldChange
+  names(CR.foldchanges) <- ChIP_Seq_and_RNA_Seq_overlap$entrez
+  head(CR.foldchanges)
+  keggres.sigmet.idx <- gage(CR.foldchanges,
+                             gsets    = kegg.sigmet.idx,
+                             same.dir = TRUE)
+  lapply(keggres.sigmet.idx, head,10)
+  fc.go.bp.p <- gage(CR.foldchanges,
+                     gsets    = go.bp.gs,
+                     same.dir = TRUE)
+  lapply(fc.go.bp.p, head,20)
+  write.table(fc.go.bp.p$greater,
+              file = "ChIP_Seq_and_RNA_Seq_overlap.go.bp.p.greater.txt",
+              sep = "\t")
+  write.table(fc.go.bp.p$less,
+              file = "ChIP_Seq_and_RNA_Seq_overlap.go.bp.p.less.txt",
+              sep = "\t")
 }
+doAll2a()
