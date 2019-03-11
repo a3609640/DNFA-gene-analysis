a278626f08ba376e7e2ec209e2daa8a57b5dbddd
Su Wu <wusu2014@gmail.com>
2019-03-10 23:12:32 -0400

    add packages

--- R/EIFproteomics.R
+++ R/EIFproteomics.R
@@ -1,3 +1,39 @@
+library(AnnotationDbi)
+library(BiocStyle)
+library(cluster)
+library(colorspace)
+library(DESeq2)
+library(Biobase)
+library(BiocGenerics)
+library(plyr)  # deliberately out of sorted order; must precede dplyr
+library(dplyr)
+library(factoextra)
+library(FactoMineR)
+library(gage)
+library(gageData)
+library(geneplotter)
+library(genefilter)
+library(GenomeInfoDb)
+library(GenomicRanges)
+library(ggplot2)
+library(gplots)
+library(IRanges)
+library(lattice)
+library(Matrix)
+library(org.Hs.eg.db)
+library(parallel)
+library(pheatmap)
+library(plotly)
+library(rmarkdown)
+library(Rcpp)
+library(RcppArmadillo)
+library(RColorBrewer)
+library(S4Vectors)
+library(SummarizedExperiment)
+library(stats4)
+library(stringr)
+library(survival)
+
 EIF.proteomics <- read.csv(file.path("project-data", 
                                      "proteomics.csv"), 
                            header = TRUE, 
@@ -18,3 +54,4 @@ dds <- DESeqDataSetFromMatrix(countData = guideData,
                               design    = ~ condition)
 dds
 head(assay(dds))
+
