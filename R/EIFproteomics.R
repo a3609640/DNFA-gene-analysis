594bec5eedc7fbadc84ec07c85dbd439a7d76bf8
Su Wu <wusu2014@gmail.com>
2019-03-14 17:55:10 -0400

    add scripts to generate heatmap

--- R/EIFproteomics.R
+++ R/EIFproteomics.R
@@ -2,6 +2,7 @@ library(AnnotationDbi)
 library(BiocStyle)
 library(cluster)
 library(colorspace)
+library(ComplexHeatmap)
 library(DESeq2)
 library(Biobase)
 library(BiocGenerics)
@@ -19,6 +20,7 @@ library(ggplot2)
 library(gplots)
 library(IRanges)
 library(lattice)
+library(made4)
 library(Matrix)
 library(org.Hs.eg.db)
 library(parallel)
@@ -45,13 +47,29 @@ nlevels(EIF.proteomics$gene_id)
 EIF.proteomics <- EIF.proteomics[!duplicated(EIF.proteomics$gene_id), ]
 EIF.proteomics2 <- EIF.proteomics[ ,-1]
 rownames(EIF.proteomics2) <- EIF.proteomics[ ,1]
-guideData <- EIF.proteomics2
-condition <- c("X60", "X40", "X20", "X10", "X5")
-guideDesign <- data.frame(row.names = colnames(guideData),
-                          condition = condition)
-dds <- DESeqDataSetFromMatrix(countData = guideData,
-                              colData   = guideDesign,
-                              design    = ~ condition)
-dds
-head(assay(dds))
+guide.EIF.Data <- EIF.proteomics2
+EIF.condition <- c("60 uM", "40 uM", "20 uM", "10 uM", "5 uM")
+guide.EIF.Design <- data.frame(row.names = colnames(guide.EIF.Data),
+                               condition = EIF.condition)
+
+
+dists <- dist(t(guide.EIF.Data))
+plot(hclust(dists), labels = guide.EIF.Design$condition)
+sampleDistMatrix <- as.matrix(dists)
+rownames(sampleDistMatrix) <- paste("4EGi", EIF.condition, sep = "-")
+colnames(sampleDistMatrix) <- paste("4EGi", EIF.condition, sep = "-")
+colors <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows = dists,
+         clustering_distance_cols = dists,
+         col                      = colors)
+
+
+Heatmap(guide.EIF.Data)
+EIFMatrix = data.matrix(guide.EIF.Data)
+EIFMatrix = EIFMatrix[sample(nrow(EIFMatrix), nrow(EIFMatrix)), 
+                      sample(ncol(EIFMatrix), ncol(EIFMatrix))]
+Heatmap(EIFMatrix)
+
+
 
